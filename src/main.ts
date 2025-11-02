3f89da7e987d7e4a85f5c892a3977068d0ae6f2c
Aisa Berro <aisa.berro@gmail.com>
2025-11-02 23:12:08 +0100

    show first page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { addIcons } from 'ionicons';
+import { sparklesOutline } from 'ionicons/icons';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+addIcons({ 'sparkles-outline': sparklesOutline });
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
